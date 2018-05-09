d74116b3dd0ec12d08aa4c30c0ead66f5a1fe907
respublika <teglas.szilvia@gmail.com>
2018-05-09 20:33:15 +0200

    Add Comparator-Domino part to week6/day2

--- week-06/day-02/Comparator/src/Domino.java
+++ week-06/day-02/Comparator/src/Domino.java
@@ -1,6 +1,5 @@
-import java.util.Arrays;
 
-public class Domino {
+public class Domino implements Comparable<Domino> {
     private final int[] values;
 
     public Domino(int valueA, int valueB) {
@@ -14,5 +13,31 @@ public class Domino {
     @Override
     public String toString() {
         return "[" + values[0] + ", " + values[1] + "]";
+    }
+
+    public int compareTo(Domino domino) {
+        int result=0;
+        if (this.getValues()[0]>domino.getValues()[0]) {
+            result=1;
+        }
+
+        if (this.getValues()[0]<domino.getValues()[0]) {
+            result=-1;
+        }
+
+        if (this.getValues()[0]==domino.getValues()[0]) {
+            if (this.getValues()[1]>domino.getValues()[1]) {
+                result=1;
+            }
+
+            if (this.getValues()[1]<domino.getValues()[1]) {
+                result=-1;
+            }
+
+            if (this.getValues()[1]<domino.getValues()[1]) {
+                result=0;
+            }
+        }
+    return result;
     }
 }
