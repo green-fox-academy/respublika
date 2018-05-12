b5db04bcba3d0b95811fca14b8bc995175cf807b
respublika <teglas.szilvia@gmail.com>
2018-05-12 14:27:59 +0200

    Add Animal to week5/day1

---
+++ week-05/day-01/Animal/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal=new Animal();
+
+        animal.getValues();
+        animal.play();
+        animal.play();
+        animal.play();
+        animal.getValues();
+        animal.eat();
+        animal.eat();
+        animal.drink();
+        animal.getValues();
+
+    }
+
+
+
+
+}
